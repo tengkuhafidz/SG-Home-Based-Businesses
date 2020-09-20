f05dae34ca34fce3a3f28ef5556fcefacd0b9f8d
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-09-20 08:42:05 +0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/camelcase */
+module.exports = {
+  siteMetadata: {
+    title: 'SG Home-Based Businesses',
+    titleTemplate: '%s',
+    description: 'Directory of Home-Based businesses in Singapore',
+    author: 'tengkubros',
+    url: 'https://fidz.dev', // No trailing slash allowed!
+    image: '/app-banner.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: 'tengkubros',
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true, // Enable tailwindcss support
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `SG Home-Based Businesses`,
+        short_name: `SG HBBs`,
+        start_url: `/`,
+        background_color: `#F7FAFC`,
+        theme_color: `#2B6CB0`,
+        display: `standalone`,
+        icon: `static/app-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
